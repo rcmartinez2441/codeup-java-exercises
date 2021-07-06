6ec9c570c54cbe95beb3c9a12feb5869fe1b3d67
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-07-06 12:57:22 -0600

    finished java quiz

---
+++ src/JavaQuizTesting.java
@@ -0,0 +1,12 @@
+public class JavaQuizTesting {
+    public static void main(String[] args) {
+        int z = 5 * 4 % 3;
+        System.out.println(z);
+
+        int x = 5;
+       System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7);
+
+        String test = ""+4;
+        System.out.println(test);
+    }
+}
