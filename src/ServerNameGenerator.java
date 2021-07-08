1f93d51ed348672e7260cef80c2acf99f2627ad4
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-07-08 15:47:24 -0600

    working on more Java Arrays

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -2,6 +2,13 @@ import java.util.Random;
 
 public class ServerNameGenerator {
     public static void main(String[] args) {
+
+        //2. Server Name Generator
+        // We are going to build a server name generator. Create a class inside of src named ServerNameGenerator, and follow the specs below.
+        //Create two arrays whose elements are strings: one with at least 10 adjectives, another with at least 10 nouns.
+        //Create a method that will return a random element from an array of strings.
+        //Add a main method, and inside of your main method select and random noun and adjective and hyphenate the combination and display the generated name to the user.
+
         String[] nouns = {"Apple", "Baseball", "Chicken", "Cookie",
                 "Window", "Spring", "Star", "Leaf", "Bridge", "Cake"};
         String[] adjectives = {"Gentle", "Brave", "Jolly", "Amused",
--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -4,7 +4,10 @@ public class Movie {
     private String name;
     private String category;
 
-    public Movie() {}
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
 
     public String getName() {
         return name;
