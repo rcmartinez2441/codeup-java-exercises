85c7a328dbed48e1223f951de56f3c14cbf68e9f
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-07-12 10:48:01 -0600

    getting some more knowledge regarding treemap and Map.Entry

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -3,10 +3,7 @@ package grades;
 import com.sun.jdi.request.StepRequest;
 import util.Input;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.*;
 
 public class GradesApplication {
     public static void main(String[] args) {
@@ -55,7 +52,7 @@ public class GradesApplication {
         //Print the list of GitHub usernames out to the console, and ask the user which student they would like to see more information about. The user should enter a GitHub username (i.e. one of the keys in your map). If the given input does not match up with a key in your map, tell the user that no users with that username were found. If the given username does exist, display information about that student, including their name and their grades.
 
         //After the information is displayed, the application should ask the user if they want to continue, and keep running so long as the answer is yes.
-
+        viewAllGrades(students);
         consoleInterface(students);
     }
 
@@ -98,11 +95,17 @@ public class GradesApplication {
         //
         //        Goodbye, and have a wonderful day!
     }
-//    public static void
 
     public static void showListOfUsers (HashMap<String, Student> hashmap, Input applicationInput) {
         System.out.println("Here are the GitHub usernames of our students: \n");
-        System.out.println(hashmap.keySet() + "\n");
+        Map<String, Student> map = hashmap;
+        TreeMap<String, Student> sortedMap = new TreeMap<>();
+        sortedMap.putAll(map);
+
+        for (Map.Entry<String, Student> entry : sortedMap.entrySet()){
+            System.out.print("|" + entry.getKey() + "| ");
+        }
+        System.out.println("\n");
         whatStudent(hashmap, applicationInput);
     }
 
@@ -116,7 +119,7 @@ public class GradesApplication {
         if (hashmap.containsKey(userInput)){
             showStudentDetails(userInput, hashmap, applicationInput);
         } else {
-            System.out.println("That's not a valid entry");
+            System.out.println("\nThat's not a valid entry\n");
             tryAgain(hashmap, applicationInput);
         }
     }
@@ -126,15 +129,30 @@ public class GradesApplication {
         System.out.println("Student Name: " + hashmap.get(userTyped).getName());
         System.out.println("Github Username: " + userTyped);
         System.out.println("Grade Average: " + hashmap.get(userTyped).getGradeAverage());
+        System.out.println();
         tryAgain(hashmap, applicationDetails);
     }
 
     public static void tryAgain (HashMap<String, Student> hashmap, Input userInput){
-        boolean continueApp = userInput.yesNo("Would you like to see another Student");
+        boolean continueApp = userInput.yesNo("Would you like to see another Student? Y/N \n");
         if(continueApp){
             consoleInterface(hashmap);
         } else {
-            System.out.println("Goodbye, have a good day");
+            System.out.println("\nGoodbye, have a good day");
         }
+    }
+
+    public static void viewAllGrades (HashMap<String,Student> hashmap){
+        Map<String, Student> map = hashmap;
+        TreeMap<String, Student> treemap = new TreeMap<>();
+        treemap.putAll(map);
+        System.out.println(treemap);
+        //Map.Entry<K, V> just allows you to store a key/value data set and store it as an object, almost like an object in an array in JS
+        for (Map.Entry<String, Student> student : treemap.entrySet()){
+            Student currentStudent = treemap.get(student.getKey()); //student.getKey will return current iteration's key (AKA github username) in string and then tree.get will use that to find VALUE in Student obj for .getName()
+            System.out.println(currentStudent.getName());
+            System.out.println(currentStudent.getGrades());
+        }
+
     }
 }
