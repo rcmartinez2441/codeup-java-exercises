5cf640b32c674a4b9f27714489fe98c2e16aa68d
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-07-15 14:22:00 -0600

    working with geting path to add files and directories

--- src/fileIO/FileIO.java
+++ src/fileIO/FileIO.java
@@ -7,26 +7,31 @@ import java.nio.file.Paths;
 
 public class FileIO {
     public static void main(String[] args) {
-//        System.out.println(getFilePath("src","fileIO","test.txt").toAbsolutePath());
+//        System.out.println(getPath("src","fileIO","test.txt").toAbsolutePath());
 //        System.out.println("Does Path Exist");
 //        System.out.println(
 //                doesPathExist(
-//                        getFilePath("src", "fileIO"/*, "test.txt"*/)
+//                        getPath("src", "fileIO"/*, "test.txt"*/)
 //                )
 //        );
+        Path path = getPath("src", "fileIO", "files");
+        tryCreateDirectory(path);
 
-        tryCreatDirectory(getFilePath("src", "fileIO", "test.txt"));
+        path = Paths.get(path.toString(), "test.txt");
+
+        System.out.println(path.toAbsolutePath());
+
     }
-
-    public static Path getFilePath(String fileName) {
+    
+    public static Path getPath(String fileName) {
         return Paths.get(fileName);
     }
 
-    public static Path getFilePath(String parentDirectory, String fileName) {
+    public static Path getPath(String parentDirectory, String fileName) {
         return Paths.get(parentDirectory, fileName);
     }
 
-    public static Path getFilePath(String parentDirectory, String childDirectory, String fileName) {
+    public static Path getPath(String parentDirectory, String childDirectory, String fileName) {
         return Paths.get(parentDirectory, childDirectory, fileName);
     }
 
@@ -36,7 +41,7 @@ public class FileIO {
     }
 
     //this method will create a directory
-    public static void tryCreatDirectory(Path pathToCreate) {
+    public static void tryCreateDirectory(Path pathToCreate) {
         if (!doesPathExist(pathToCreate)) {
             try {
                 Files.createDirectory(pathToCreate);
@@ -46,6 +51,19 @@ public class FileIO {
             }
         } else {
             System.out.println("The path at " + pathToCreate.toAbsolutePath() + " already exists");
+        }
+    }
+
+    public static void tryCreateFile (Path path){
+        if(!doesPathExist(path)){
+            try {
+                Files.createFile(path);
+            }catch (IOException e){
+                System.out.println("Could Not Create File at: ");
+                System.out.println(path.toAbsolutePath());
+            }
+        } else {
+            System.out.println("File at " + path.toAbsolutePath() + "Already Exists");
         }
     }
 
